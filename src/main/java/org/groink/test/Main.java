e6bdf5ad95b3c32f25f55c6934e47f1caa7415b4
Groinkb <benjamin@mossu.ch>
2024-10-11 15:02:22 +0200

    Finish the event "PlayerEggThrowEvent" and the event "PlayerMoveEvent"

--- src/main/java/org/groink/test/Main.java
+++ src/main/java/org/groink/test/Main.java
@@ -2,8 +2,10 @@ package org.groink.test;
 
 import com.groink.test.Events;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +18,14 @@ public final class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e){
-        e.getTo()
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.GREEN +"stopmoving petit pied");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e){
+
+        e.getPlayer().sendMessage(ChatColor.YELLOW+"Tu as lancé un oeuf tête de noeil");
+
     }
 }
