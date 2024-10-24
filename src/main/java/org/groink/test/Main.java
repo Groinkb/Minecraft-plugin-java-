374ed577880e4f60153e4a8078251f790c16a82f
Groinkb <benjamin@mossu.ch>
2024-10-24 16:52:19 +0200

    Fix: Corrected plugin.yml syntax and schema validation issues
    - Fixed api-version format (changed 'api-version: 1.20' to '1.20')
    - Expanded 'commands' section for 'heal' with description, usage, permission, and permission-message
    - Ensured proper YAML formatting to resolve 'Schema validation: Incompatible types' error

--- src/main/java/org/groink/test/Main.java
+++ src/main/java/org/groink/test/Main.java
@@ -1,32 +1,12 @@
 package org.groink.test;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerEggThrowEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Enregistre la classe Main comme Listener pour capter les événements
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
+        getCommand("test").setExecutor(new TestCommand());
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e){
-        // Empêche le joueur de bouger
-        e.setCancelled(true);
-        // Envoie un message au joueur
-        e.getPlayer().sendMessage(ChatColor.GREEN + "stopmoving petit pied");
-    }
-
-    @EventHandler
-    public void onPlayerEggThrow(PlayerEggThrowEvent e){
-        // Envoie un message au joueur qui a lancé un œuf
-        e.getPlayer().sendMessage(ChatColor.YELLOW + "Tu as lancé un oeuf tête de noeil");
     }
 }
---
+++ src/main/java/org/groink/test/TestCommand.java
@@ -0,0 +1,27 @@
+package org.groink.test;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TestCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            // is first argument is equal to 'hello', then hello back
+            if (args.length == 1) {
+                if (args [0].equalsIgnoreCase("hello")) {
+                    ((Player) sender).sendMessage("Hello my friend");
+                }
+            }
+
+
+        }
+
+
+        return false;
+    }
+}
